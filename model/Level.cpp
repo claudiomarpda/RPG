a98785a46184dad97c78cc1eacec3776ab7212e4
Zaaraki <amaro.euclides@gmail.com>
2017-06-01 08:34:49 -0300

    Level and Weapon classes implemented

---
+++ model/Level.cpp
@@ -0,0 +1,51 @@
+//
+// Created by amaro on 25/05/2017.
+//
+
+#include "Level.h"
+
+
+int Level::getNextLvl() const {
+    return nextLvl;
+}
+
+void Level::setNextLvl(int nextLevel) {
+    Level::nextLvl = nextLevel;
+}
+
+const int Level::getMIN() {
+    return MIN;
+}
+
+const int Level::getMAX() {
+    return MAX;
+}
+
+int Level::getExp() const {
+    return exp;
+}
+
+void Level::setExp(int exp) {
+    Level::exp = exp;
+}
+
+int Level::getLvl() const {
+    return lvl;
+}
+
+void Level::setLvl(int lvl) {
+    Level::lvl = lvl;
+}
+
+int Level::levelUp(int expBar, int lvl) {
+    lvl+=1;
+    nextLvl = nextLvl*2;
+
+}
+
+void Level::addExp(int exp){
+    int aux=0;
+    aux+=exp;
+    if(aux >= nextLvl)
+        levelUp(nextLvl, lvl);
+}
